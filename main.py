e22e84a554fb9602354b20aa21e59948f1aad1a5
marti1999 <mcaixal1999@gmail.com>
2021-12-12 20:17:55 +0100

    reading data

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-def main():
-    print("hola")
+import os
+import re
+import string
+import math
+import pandas
 
+
+def get_data():
+    data = pandas.read_csv('top10k.csv', sep=';')
+    X = data['tweetText']
+    y = data['sentimentLabel']
+    # return X.values, y.values
+    return X.tolist(), y.tolist()
+
+
+
+def main():
+    X, y = get_data()
+    print("hola")
 if __name__ == "__main__":
     main()
