4b84390ee1ebd23064f1e8e786d448e06b7314ab
marti1999 <mcaixal1999@gmail.com>
2021-12-12 22:55:30 +0100

    predict function created

--- main.py
+++ main.py
@@ -16,13 +16,14 @@ def get_data():
     # return X.tolist(), y.tolist()
     return X, y
 
+
 class naiveBayes():
     def __init__(self):
 
-        self.tweet_num = {} # stores the amount of tweets that are 'positive' or 'negative'
-        self.log_class_priors = {} # stores the prior probability of a message being 'positive' or 'negative'
-        self.wc = {} # for each class, stores the amount of times a word appears
-        self.vocab = set() # set of unique words.
+        self.tweet_num = {}  # stores the amount of tweets that are 'positive' or 'negative'
+        self.log_prior_probability = {}  # stores the log prior probability of a message being 'positive' or 'negative'
+        self.wc = {}  # for each class, stores the amount of times a word appears
+        self.dictionary = set()  # set of unique words.
 
     def item_count(self, text):
         wc = {}
@@ -47,8 +48,8 @@ class naiveBayes():
         self.tweet_num['negative'] = n - self.tweet_num['positive']
 
         # calculating the prior probability of a message being 'positive' or 'negative'
-        self.log_class_priors['negative'] = math.log(self.tweet_num['negative'])
-        self.log_class_priors['positive'] = math.log(self.tweet_num['positive'])
+        self.log_prior_probability['negative'] = math.log(self.tweet_num['negative'])
+        self.log_prior_probability['positive'] = math.log(self.tweet_num['positive'])
 
         self.wc['negative'] = {}
         self.wc['positive'] = {}
@@ -67,21 +68,58 @@ class naiveBayes():
             for word, count in counts.items():
 
                 # creating new items should they not exist
-                if word not in self.vocab:
-                    self.vocab.add(word)
-                if  word not in self.wc[c]:
+                if word not in self.dictionary:
+                    self.dictionary.add(word)
+                if word not in self.wc[c]:
                     self.wc[c][word] = 0.0
 
-                # increasing the count for wrod in class
+                # increasing the count for word in class
                 self.wc[c][word] += count
 
+    def predict(self, X):
+        xArr = X.values()
+        result = []
 
-        return n
+        for tweet in xArr:
+            word_counts = self.item_count(self.split(tweet))
+            positive_count = 0
+            negative_count = 0
+            for word, _ in word_counts.items():
+                if word not in self.dictionary:
+                    continue
+
+                # Applying Naive Bayes
+                # We need to calculate p(w_i | positive) and p(w_i | negative)
+                # The numerator is how many times w_i appears in a tweet of such class, divided by the count of
+                # all words in the tweets of the class.
+                # Since we can't calculate log of 0, we use Laplace Smoothing, adding 1 to the numerator.
+                # In order to balance it, the size of the dictionary must be added to the numerator
+                log_positive = math.log((self.wc['positive'].get(word, 0.0) + 1)
+                                        / (self.tweet_num['positive'] + len(self.dictionary)))
+                log_negative = math.log((self.wc['negative'].get(word, 0.0) + 1)
+                                        / (self.tweet_num['negative'] + len(self.dictionary)))
+
+                positive_count += log_positive
+                negative_count += log_negative
+
+            # Adding prior probability of each class
+            positive_count += self.log_prior_probability['positive']
+            negative_count += self.log_prior_probability['negative']
+
+            if positive_count > negative_count:
+                result.append(1)
+            else:
+                result.append(0)
+
+        return result
+
 
 def main():
     X, y = get_data()
     nb = naiveBayes()
     nb.fit(X, y)
     print("hola")
+
+
 if __name__ == "__main__":
     main()
